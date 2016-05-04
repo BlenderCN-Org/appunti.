cc4812797380da350f0399b1299e65d030c6a69b
MaoZ75 <comeunbalcone@gmail.com>
2016-05-04 02:46:26 +0200

    render data ora

--- python/blender_tests.py
+++ python/blender_tests.py
@@ -6,10 +6,17 @@ Created on Fri Apr 29 23:36:46 2016
 """
 
 import os
+import time
 try:
-    import bpy
+    import bpy, bpy.data, bpy.ops #, bpy.props, bpy.types, bpy.context, bpy.utils, bgl, blf, mathutils
 except ImportError:
+    print ('not imported bpy')
     pass
+
+"""
+import importlib
+importlib.reload(blender_tests)
+"""
 
 import sys
 sys.path.append("/home/maurizio/GitBook/Library/maoz75/gli-appunti/python")
@@ -33,12 +40,32 @@ def load_all_in_blender():
         use_shadeless=True, use_transparency=True, transparency_method='Z_TRANSPARENCY', alpha_mode='PREMUL', 
         relative=False)
     set_camera_down()
+    
+def load_image_list_in_blender(image_list=['vajrasana', 'shashankasana', 'adho_mukha_svanasana', 'uttanasana', 'baddha_konasana', 'upavishta_konasana', 'janu_sirsasana', 'janu_sirsasana', 'paschimottanasana', 'shavasana', 'halasana', 'sarvangasana', 'utthita_sarvangasana', 'utthita_sarvangasana', 'karnapidasana', 'halasana', 'shavasana', 'supta_baddha_konasana', 'shavasana'],
+    base_dir="/home/maurizio/GitBook/Library/maoz75/gli-appunti/figures/asana_yoga/"):
+    filelist=[]
+    for img in image_list:
+        filelist.append({'name':"{}.jpg".format(img)})
+    print(filelist)
+    bpy.ops.import_image.to_plane(files=filelist,
+        directory=base_dir, 
+        filter_image=True, filter_movie=True, filter_glob="", force_reload=True, 
+        use_shadeless=True, use_transparency=True, transparency_method='Z_TRANSPARENCY', alpha_mode='PREMUL', 
+        relative=False)
+    
 
 def load_sequence(list_of_files):
     list_of_files_for_planes=[]
     for l in list_of_files:
         list_of_files_for_planes.append({'name':l})
     return list_of_files_for_planes
+
+def insert_text(name='Text01', text='Text'):
+    myFontCurve = bpy.data.curves.new(type="FONT",name="myFontCurve")
+    myFontOb = bpy.data.objects.new(name,myFontCurve)
+    myFontOb.data.body = text
+    bpy.context.scene.objects.link(myFontOb)
+    bpy.context.scene.update()    
 
 def select_camera():    
     bpy.ops.object.select_pattern(pattern="Camera")
@@ -60,37 +87,42 @@ def some_uts():
     bpy.data.objects['Camera'].location[0] = 0
 
 
-def create_timeline(translation_x = -1.77):
-    waits = [] # list transient in
-    transactions = [] # list of pauses
-    steps = 12
-    for u in range(steps):
-        waits.append(20)
-        transactions.append(20)
-    time_index = 0
-    for scene in range(steps):
+def create_timeline(secs = [120, 180, 60, 120, 60, 120, 60, 60, 240, 60, 30, 60, 60, 60, 30, 30, 30, 300, 300],
+                    transition_time = 1,
+                    fps=24,
+                    translation_x=-1.77):
+    # Standstill for transition_time frames
+    time_index = transition_time * fps
+    timing_scenes = [time_index]
+    bpy.context.scene.frame_current = time_index
+    bpy.ops.anim.keyframe_insert_menu(type='__ACTIVE__', confirm_success=True)
+    for scene in range(len(secs)):
+        # Set keyframe on end scene
+        time_index += secs[scene] * fps
         bpy.context.scene.frame_current = time_index
         bpy.ops.anim.keyframe_insert_menu(type='__ACTIVE__', confirm_success=True)
-        time_index += waits[scene]
+        # Set keyframe on end transition
+        time_index += transition_time * fps
+        timing_scenes.append(time_index)
         bpy.context.scene.frame_current = time_index
         bpy.ops.anim.keyframe_insert_menu(type='__ACTIVE__', confirm_success=True)
-        bpy.ops.transform.translate(value=(translation_x, 0, 0))
-        time_index += transactions[scene]
-    bpy.context.scene.frame_end = 481
-    bpy.context.scene.file_format = 'H264'
-    bpy.context.scene.render.filepath = "/tmp/tem3.avi"
+        bpy.ops.transform.translate(value=(translation_x, 0, 0)) 
+        time.sleep(.5)
+    bpy.context.scene.frame_end = time_index
+    # bpy.context.scene.file_format = 'H264'
+    bpy.context.scene.render.filepath = "/tmp/temp4.avi"
     bpy.context.scene.render.resolution_x = 640
     bpy.context.scene.render.resolution_y = 360
-
+    
 
 
 def right_sequence():
     blender_tests.set_camera_down(bpy.data.objects['Camera'])
-    blender_tests.set_camera_down(bpy.data.objects['Lamp'])
+    # blender_tests.set_camera_down(bpy.data.objects['Lamp'])
     bpy.data.objects['Camera'].select=True
-    bpy.data.objects['Lamp'].select=True    
-    blender_tests.load_all_in_blender()
-    #create_timeline()
+    # bpy.data.objects['Lamp'].select=True    
+    blender_tests.load_image_list_in_blender()
+    create_timeline()
 
     
     
--- python/generate_tables.py
+++ python/generate_tables.py
@@ -15,33 +15,66 @@ def find_description(body,title):
         return body[start:end]
     else:
         return "ciccia"
+
+def generate_table(txt_commands="vajrasana 120, shashankasana 180, adho_mukha_svanasana 60, uttanasana 120, baddha_konasana 60, upavishta_konasana 120, janu_sirsasana 60x2, paschimottanasana 240, shavasana 60, halasana 30, sarvangasana 60, utthita_sarvangasana 60x2, karnapidasana 30, halasana 30, shavasana 30, supta_baddha_konasana 300, shavasana 300", 
+                   orig_adoc='/home/maurizio/GitBook/Library/maoz75/gli-appunti/14_yoga.adoc'):
+    f=open(orig_adoc)
+    orig_adoc = f.read()
+    f.close()
+    listed_table = txt_commands.split(', ')
+    adoc_table = """.Tabella
+    [header=yes, cols="^1,2,1"]
+    |===
+    | Posizione | Descrizione | Secondi
+    """
+    images=[]
+    secs=[]
+    descs=[]
+    for elem in listed_table:
+        image, sec = elem.split(' ')
+        desc = find_description(orig_adoc,image.replace('_', ' '))
+        pos_x = sec.find('x') # -1 not found
+        times = 0 
+        if pos_x > -1:
+            times = int(sec[pos_x+1:])
+            sec = sec[0:pos_x]
+        images.append(image)
+        descs.append(desc)
+        secs.append(int(sec))
+        adoc_table += "| image:figures/asana_yoga/{}.svg[role=right, pdfwidth=5cm] | {} | {} \n".format(image, desc, sec)
+        for i in range(times-1):
+            adoc_table += "| image:figures/asana_yoga/{}.svg[role=right, pdfwidth=5cm] | altro lato | {} \n".format(image, sec)
+            images.append(image)
+            descs.append('Cambia Lato')
+            secs.append(int(sec))
+    adoc_table += "|===\n"
+    return images, descs, secs
     
 
-f=open('/home/maurizio/GitBook/Library/maoz75/gli-appunti/14_yoga.adoc')
-s = f.read()
-f.close()
+def generate_srt_text(images, descs, secs, transition=1):
+    """
+    A numeric counter identifying each sequential subtitle
+    The time  (00:00:00,000) that the subtitle should appear on the screen, followed by --> and the time it should disappear
+    Subtitle text itself on one or more lines
+    A blank line containing no text, indicating the end of this subtitle[9]
+    """
+    srt_text = ""
+    act_time = transition
+    for i in range (len(images)):        
+        srt_text+="{:02d}\n00:{:02d}:{:02d},000-->00:{:02d}:{:02d},000\n{}: {}\n\n".format(
+            i+1, 
+            int(act_time/60), act_time%60, 
+            int((act_time+secs[i])/60), (act_time+secs[i])%60, 
+            images[i], descs[i])
+        act_time+=secs[i]
+    return srt_text
 
-elenco = "vajrasana 120, shashankasana 180, adho_mukha_svanasana 60, uttanasana 120, baddha_konasana 60, upavishta_konasana 120, janu_sirsasana 60x2, paschimottanasana 240, shavasana 60, halasana 30, sarvangasana 60, utthita_sarvangasana 60x2, karnapidasana 30, halasana 30, shavasana 30, supta_baddha_konasana 300, shavasana 300"
-lista = elenco.split(', ')
+    
+images, descs, secs = generate_table()
+print(images, descs, secs )
+print generate_srt_text(images, descs, secs)
 
 
-tabella = """.Tabella
-[header=yes, cols="^1,2,1"]
-|===
-| Posizione | Descrizione | Secondi
-"""
-for elem in lista:
-    image, secs = elem.split(' ')
-    pos_x = secs.find('x') # -1 not found
-    desc = find_description(s,image.replace('_', ' '))
-    if pos_x > -1:
-        times = int(secs[pos_x+1:])
-        new_secs = secs[0:pos_x]
-        tabella += "| image:figures/asana_yoga/{}.svg[role=right, pdfwidth=5cm] | {} | {} \n".format(image, desc, new_secs)
-        for i in range(times-1):
-            tabella += "| image:figures/asana_yoga/{}.svg[role=right, pdfwidth=5cm] | altro lato | {} \n".format(image, new_secs)
-    else:
-        tabella += "| image:figures/asana_yoga/{}.svg[role=right, pdfwidth=5cm] | {} | {} \n".format(image, desc, secs)
-tabella += "|===\n"
-print tabella
+        
+
 
